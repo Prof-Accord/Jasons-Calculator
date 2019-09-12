bd3ff87e22d3489136a1eb35a5a3a03f90831e4f
Ubuntu <ubuntu@ip-172-31-41-22.us-east-2.compute.internal>
2019-09-12 04:11:27 +0000

    remove corrrect code

--- app.rb
+++ app.rb
@@ -2,31 +2,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'date'
 
-@today = Date.today
-
 get '/' do
   @title = "Home"
   @today = Date.today
   erb :index
 end
 
-get '/result' do
+post '/result' do
   @title = "Result"
+  @today = Date.today
+  @user_num = params[:number]
   erb :result
 end
 
-post '/result' do
-  @title = "Result"
-  @user_num = params[:number]
-  @today = Date.today
-  @count = 0
-  # until @user_num == @count do
-  #   until @today.wday == 5 && @today.mday == 13
-  #     @today += 1
-  #   end
-  #   @count += 1
-  #   break if @user_num == @count
-  #   @today += 1
-  # end
-  erb:result
-end
+# post '/result' do
+#   @title = "Result"
+#   @user_num = params[:number]
+#   @today = Date.today
+#   @count = 0
+#   # until @user_num == @count do
+#   #   until @today.wday == 5 && @today.mday == 13
+#   #     @today += 1
+#   #   end
+#   #   @count += 1
+#   #   break if @user_num == @count
+#   #   @today += 1
+#   # end
+#   erb:result
+# end
