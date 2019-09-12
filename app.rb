558c76c550b8dfc4c4d67bd90c81881a97ded9c4
Ubuntu <ubuntu@ip-172-31-41-22.us-east-2.compute.internal>
2019-09-12 04:52:24 +0000

    modify code

--- app.rb
+++ app.rb
@@ -11,23 +11,13 @@ end
 post '/result' do
   @title = "Result"
   @today = Date.today
+  @count = 0
   @user_num = params[:number]
-  @user_num += 1
   erb :result
 end
 
 # post '/result' do
 #   @title = "Result"
-#   @user_num = params[:number]
-#   @today = Date.today
-#   @count = 0
-#   # until @user_num == @count do
-#   #   until @today.wday == 5 && @today.mday == 13
-#   #     @today += 1
-#   #   end
-#   #   @count += 1
-#   #   break if @user_num == @count
-#   #   @today += 1
 #   # end
 #   erb:result
 # end
