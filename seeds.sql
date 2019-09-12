228d73707d81502a0135fa58897d969dff5a200d
Ubuntu <ubuntu@ip-172-31-41-22.us-east-2.compute.internal>
2019-09-12 02:07:38 +0000

    delete sqlite3

--- seeds.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists jason_days;
-create table jason_days (
-  id integer primary key,
-  year integer,
-  month integer,
-  day integer
-);
-
-insert into jason_days (year, month, day) values (2019, 09, 13);
